57e9311cdd60cff548819b2af693ac6105047159
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-27 20:41:15 +0530

    adding Dependencies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -37,7 +38,16 @@ app.get("/compose", function (req, res) {
 });
 
 app.get("/posts/:postName", function (req, res) {
-  res.send(req.params.postName);
+  const requestedTitle = req.params.postName;
+  Post.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    const dataTitle = _.lowerCase(requestedTitle);
+    if (storedTitle == dataTitle) {
+      console.log("Match Found");
+    } else {
+      console.log("No Match Found");
+    }
+  });
 });
 
 app.post("/compose", function (req, res) {
