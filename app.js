8c9067ec4bf882198d4da160f36ff13c9292cbff
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-28 12:54:10 +0530

    Ready to Deploy

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 mongoose.connect(
-  "mongodb+srv://admin-Nikhil:Database-123@cluster0-hljq8.mongodb.net/blogDB",
+  "mongodb+srv://Nikhil:Database-123@cluster0-bvnfk.mongodb.net/blogDB",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
